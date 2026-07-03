c4263d070955036968d618e82ca9dc40d3168a35
vidhiverma23 <vidhivrma23@gmail.com>
2026-07-03 12:33:34 +0530

    Add linked list implementation in Java

---
+++ LinkedList.java
@@ -0,0 +1,20 @@
+public class LinkedList {
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static void main(String[] args){
+        Node head=new Node(10);
+        head.next=new Node(20);
+        head.next.next=new Node(30);
+        Node temp=head;
+        while(temp!=null){
+            System.out.println(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+}
